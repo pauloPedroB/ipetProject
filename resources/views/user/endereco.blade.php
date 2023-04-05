de308ddcdbcbe51975ea979789fdf07f2cc46aa0
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-04-05 20:07:31 -0300

    .

--- resources/views/user/endereco.blade.php
+++ resources/views/user/endereco.blade.php
@@ -82,11 +82,11 @@
             </div>
             <div class="form-group">
                 <label for="title">Latitude</label>
-                <input type="text" class="form-control" id="lat" name="lat" placeholder="Sua Latitude..." value="{{$Latitude}}" disabled required data-input>
+                <input type="text" class="form-control" id="lat" name="lat" placeholder="Sua Latitude..." value="{{$Latitude}}" required>
             </div>
             <div class="form-group">
                 <label for="title">Latitude</label>
-                <input type="text" class="form-control" id="long" name="long" placeholder="Sua Longitude..." value="{{$Longitude}}"  disabled required data-input>
+                <input type="text" class="form-control" id="long" name="long" placeholder="Sua Longitude..." value="{{$Longitude}}" required>
             </div>
             <input type="submit" class="btn btn-primary" value="{{$btn}}">
         </form>
