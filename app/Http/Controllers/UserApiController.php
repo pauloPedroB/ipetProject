6d8d89fbfbbe72ce7a1a73905470a885532434e2
odairsp <odair.sp@hotmail.com>
2023-04-13 11:16:13 -0300

    api

--- app/Http/Controllers/UserApiController.php
+++ app/Http/Controllers/UserApiController.php
@@ -17,7 +17,7 @@ class UserApiController extends Controller
         $user = User::find('1');
 
         $data = ['id' => $user->id, "email" => $user->email];
-        return json_encode($data);
+        return json_encode($data, JSON_FORCE_OBJECT);
     }
 
     /**
