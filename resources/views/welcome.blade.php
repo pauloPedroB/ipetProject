88bff689577101081be5742e0e7ceb85be53ee54
odair <odair.sp@hotmail.com>
2023-04-19 16:12:14 -0300

    atualiza styleToogle

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -35,7 +35,7 @@
                     <img src="/img/IPetLogo.png" alt="" id="nav-logo">
                 </a>
                 <a class="navbar-brand" href="#">iPET</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
+                <button id="btnToogle" class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav"
                     aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
