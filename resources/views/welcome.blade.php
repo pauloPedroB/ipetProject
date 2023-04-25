154f7a488feedda109d827baed1ab8b14f5a4177
Odair <odair.sp@hotmail.com>
2023-04-24 21:22:58 -0300

    time carroussel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -71,14 +71,14 @@
     @endauth
 
 
-    <div id="carouselIpet" class="carousel slide">
+    <div id="carouselIpet" class="carousel slide " data-bs-ride="carousel">
         <div class="carousel-indicators">
             <button type="button" data-bs-target="#carouselIpet" data-bs-slide-to="0" class="active"
                 aria-current="true" aria-label="Slide 1"></button>
             <button type="button" data-bs-target="#carouselIpet" data-bs-slide-to="1" aria-label="Slide 2"></button>
             <button type="button" data-bs-target="#carouselIpet" data-bs-slide-to="2" aria-label="Slide 3"></button>
         </div>
-        <div class="carousel-inner">
+        <div class="carousel-inner" data-bs-interval="500">
 
             <div class="carousel-caption1">
                 <h1>Busque um Produto</h1>
