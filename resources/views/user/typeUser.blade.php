4a685f3904045e5fe5e4727d4f32d3915619df82
odairsp <odair.sp@hotmail.com>
2023-04-25 14:40:36 -0300

    Changes to be committed: 	modified:   resources/views/layouts/main.blade.php 	modified:   resources/views/user/typeUser.blade.php 	modified:   routes/web.php

--- resources/views/user/typeUser.blade.php
+++ resources/views/user/typeUser.blade.php
@@ -1,23 +1,27 @@
 @extends('layouts.main')
-@section('titlle','Tipo de Usuário')
+@section('titlle', 'Tipo de Usuário')
 @section('content')
-    <div class="row" id="cards-container">
+    <div id="container" class="col m-5 p-5 card d-flex flex-column text-center w-75 h-50">
 
         <h1>Você deseja se cadastrar como:</h1>
+        <div class=" card d-flex flex-row m-5 p-5 justify-content-center">
 
-        <div class="cardLoja card col-md-">
-            <a href="/Registar/Loja"><span class="material-symbols-outlined img-fluid" style="width: 300px;">store</span></a>
-            <p>Loja</p>
-            
-        </div>
 
-        
+            <div class="btn cardLoja card d-inline-flex p-2 m-5 p-5 ">
+                <a href="/Registra/Loja"><span class="material-symbols-outlined">store</span></a>
+                <div class="card-body">
+                    <p class="fs-1">Loja</p>
+                </div>
+            </div>
 
-        <div class="cardUsuario">
-            <a href="/Registar/Usuario"><span class="material-symbols-outlined">person</span></a>
-            <p>Usuário</p>
+
+            <div class="btn cardUsuario card d-inline-flex p-2 m-5 p-5">
+                <a href="/Registra/Usuario"><span class="material-symbols-outlined">person</span></a>
+                <div class="card-body">
+                    <p class="fs-1">Usuário</p>
+                </div>
+            </div>
         </div>
 
     </div>
 @endsection
-    
