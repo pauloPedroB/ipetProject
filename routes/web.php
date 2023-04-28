d2169ad01feb16a66e8f5c275c2b27c40ff2fd60
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-04-27 22:18:58 -0300

    a

--- routes/web.php
+++ routes/web.php
@@ -64,7 +64,7 @@ Route::get('/produto/copiar/{id}', [ProductsController::class, 'copy'])->middlew
 
 
 Route::post('/produto', [ProductsController::class, 'store'])->middleware('auth', 'CheckADM');;
-Route::get('/produtos/{id}/{prod?}', [ProductsController::class, 'show'])->where('id', '[0-9]{1,2,3}');;
+Route::get('/produto/{id}/{prod?}', [ProductsController::class, 'show'])->where('id', '[0-9]{1,2,3}');;
 Route::delete('/produtos/{id}', [ProductsController::class, 'destroy'])->middleware('auth', 'CheckLoja')->where('id', '[0-9]{1,2,3}');
 Route::get('/produtos/edit/{id}', [ProductsController::class, 'edit'])->middleware('auth', 'CheckLoja')->where('id', '[0-9]');
 Route::put('/produtos/update/{id}', [ProductsController::class, 'update'])->middleware('auth', 'CheckLoja');
