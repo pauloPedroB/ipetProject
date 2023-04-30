f68e9c102614df3b2bbf2cc214d02055903760fa
vinicius <vine.goncalo123@gmail.com>
2023-04-30 13:52:36 -0300

    estilizando a página de escolhas

--- resources/views/layouts/main.blade.php
+++ resources/views/layouts/main.blade.php
@@ -16,7 +16,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha1/dist/css/bootstrap.min.css" rel="stylesheet"
         integrity="sha384-GLhlTQ8iRABdZLl6O3oVMWSktQOp6b7In1Zl3/Jr59b6EGGoI1aFkw7cmDA6j6gD" crossorigin="anonymous">
-    <script src="/js/funcoes.js"></script>
+        <script src="/js/funcoes.js"></script>
     <script src="/js/cep.js" defer></script>
     <script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCXoIfvEDdZDSGfKCDEfcdxBoaTY1ooX-4"></script>
     <script>
@@ -101,7 +101,7 @@
     <footer>
         <p>IPET DEVELOPMENT &copy; 2023</p>
     </footer>
-
+    <script src="https://kit.fontawesome.com/0772530246.js" crossorigin="anonymous"></script>
     <script src="/js/app.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
--- resources/views/user/typeUser.blade.php
+++ resources/views/user/typeUser.blade.php
@@ -1,27 +1,21 @@
 @extends('layouts.main')
 @section('titlle', 'Tipo de Usuário')
 @section('content')
-    <div id="container" class="col m-5 p-5 card d-flex flex-column text-center w-75 h-50">
 
-        <h1 class="text-left">Você deseja se cadastrar como:</h1>
-        <div class=" card d-flex flex-row m-5 p-5 justify-content-center">
-
-        <a href="/Registrar/Loja">
-            <div class="btn cardLoja card d-inline-flex p-2 m-5 p-5 ">
-                <img class="img-fluid" class="img-thumbnail"src="/img/store_img.png" alt="" style="width: 10vw; height:15vh">
-                <div class="card-body">
+        <div class="container">
+            <div class="row justify-content-center align-items-center vh-100">
+                <div class="col-auto">
+                    <i class="fa-solid fa-user"></i>
+                    <h2>Usuário</h2>
+                </div>
+                
+                <div class="col-auto">
+                    <i class="fa-solid fa-shop"></i>
+                    <h2>Loja</h2>
                 </div>
             </div>
-        </a>
 
-        <a href="/Registrar/Usuario">
-            <div class="btn cardUsuario card d-inline-flex p-2 m-5 p-5">
-                <img class="img-fluid" src="/img/user_img.png" alt="" style="width: 10vw; height: 15vh">
-                <div class="card-body">
-                </div>
-            </div>
-        </a>
         </div>
+        
 
-    </div>
 @endsection
