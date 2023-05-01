005777eb78d4320306bb1ff1706714a00e18152f
odair <odair.sp@hotmail.com>
2023-05-01 11:59:57 -0300

    Changes to be committed: 	modified:   resources/views/user/typeUser.blade.php 	modified:   routes/web.php

--- resources/views/user/typeUser.blade.php
+++ resources/views/user/typeUser.blade.php
@@ -3,37 +3,49 @@
 @section('content')
 
 
-    {{-- container --}}
-    <div class=" container-fluid" style="background-color: #ffff">
-        
-
-        {{-- tittle --}}
-        <div class="row  justify-content-center align-items-center vh-100">
-
-            <div class="h1 d-inline-flex" style="color:#F29200"> Você deseja se cadastrar como:</div>
+{{-- container --}}
+<div class=" container-fluid" style="background-color: #ffff">
 
 
+    {{-- tittle --}}
+    <div class="row justify-content-center align-items-center vh-100">
+
+        <div class="h1 mb-1" style="color:#F29200">
+            Você deseja se cadastrar como:
+        </div>
+
+        <div class="d-flex flex-row mt-1 flex-wrap justify-content-around">
             {{-- card-market --}}
-            <div class="col-auto card-market flex-column text-center" style="margin-right: 20rem">
-                <div class="rounded-5" style="width: 15rem; height:15rem; background-color: #DBF0E1">
-                    <i class=" fa-solid fa-shop" style="font-size: 8rem; margin-top:3rem;"></i>
-                </div>  
-                <h2 class="fs-3 fw-bold flex-column" style="margin-top:2rem">Usuário</h2>                    
+
+            <div class="col-auto card-market flex-column text-center m-2 ">
+                <a class="btn" href="/Registrar/Loja">
+                    <div class="rounded-5 " style="width: 15rem; height:max-content; background-color: #DBF0E1">
+                        <i class=" fa-solid fa-shop" style="font-size: 8rem; margin-top:3rem;"></i>
+                        <h2 class="fs-3 fw-bold flex-column pb-3" style="margin-top:2rem">Loja</h2>
+                    </div>
+                </a>
             </div>
-    
-    
+
             {{-- card-user --}}
-            <div class=" col-auto card-user flex-column  text-center">
-                <div class="rounded-5" style="width: 15rem; height:15rem; background-color: #DBF0E1">
-                    <i class="fa-solid fa-user" style="font-size: 8rem; margin-top:3rem;"></i>
-                </div>
-                <h2 class="fs-3 fw-bold d-flex flex-column" style="margin-top:2rem">Loja</h2>
-    
+
+            <div class=" col-auto card-user flex-column  text-center m-2">
+                <a class="btn" href="/Registrar/Usuario">
+                    <div class="rounded-5" style="width: 15rem; height:max-content; background-color: #DBF0E1">
+                        <i class="fa-solid fa-user" style="font-size: 8rem; margin-top:3rem;"></i>
+                        <h2 class="fs-3 fw-bold  flex-column pb-3" style="margin-top:2rem">Cliente</h2>
+                    </div>
+                </a>
             </div>
+
 
         </div>
-        
+
     </div>
+
+
+</div>
+
+</div>
 
 
 @endsection
