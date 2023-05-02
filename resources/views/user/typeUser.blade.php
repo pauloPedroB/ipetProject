a93473c423172005551d5244e045904c8f9dc1c4
vinicius <vine.goncalo123@gmail.com>
2023-05-01 22:24:37 -0300

    organização da página de escolha

--- resources/views/user/typeUser.blade.php
+++ resources/views/user/typeUser.blade.php
@@ -7,31 +7,29 @@
 <div class=" container-fluid" style="background-color: #ffff">
 
 
-    {{-- tittle --}}
-    <div class="row justify-content-center align-items-center vh-100">
-
-        <div class="h1 mb-1" style="color:#F29200">
-            Você deseja se cadastrar como:
+    <div class="row " style="margin: 0">
+        
+        {{-- tittle --}}
+        <div class="p-5" style="color:#F29200; width: ">
+            <h1 class="">Você deseja se cadastrar como: </h1>
         </div>
 
         <div class="d-flex flex-row mt-1 flex-wrap justify-content-around">
             {{-- card-market --}}
-
-            <div class="col-auto card-market flex-column text-center m-2 ">
-                <a class="btn" href="/Registrar/Loja">
-                    <div class="rounded-5 " style="width: 15rem; height:max-content; background-color: #DBF0E1">
-                        <i class=" fa-solid fa-shop" style="font-size: 8rem; margin-top:3rem;"></i>
+            <div class="col-auto card-market flex-column text-center m-5 ">
+                <a class="btn" href="/Registrar/Loja" >
+                    <div class="rounded-5 icon-link icon-link-hover" style="width: 20rem; height:max-content; background-color: #DBF0E1;">
+                        <i class=" fa-solid fa-shop" style="font-size: 10rem; margin-top:3rem;"></i>
                         <h2 class="fs-3 fw-bold flex-column pb-3" style="margin-top:2rem">Loja</h2>
                     </div>
                 </a>
             </div>
 
             {{-- card-user --}}
-
-            <div class=" col-auto card-user flex-column  text-center m-2">
+            <div class=" col-auto card-user flex-column  text-center m-5">
                 <a class="btn" href="/Registrar/Usuario">
-                    <div class="rounded-5" style="width: 15rem; height:max-content; background-color: #DBF0E1">
-                        <i class="fa-solid fa-user" style="font-size: 8rem; margin-top:3rem;"></i>
+                    <div class="rounded-5" style="width: 20rem; height:max-content; background-color: #DBF0E1">
+                        <i class="fa-solid fa-user" style="font-size: 10rem; margin-top:3rem;"></i>
                         <h2 class="fs-3 fw-bold  flex-column pb-3" style="margin-top:2rem">Cliente</h2>
                     </div>
                 </a>
