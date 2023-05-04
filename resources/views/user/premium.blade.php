64be736f5a135589a868b5a3ae7d31d06bcdc2ac
Ronald <ronalddantasrodrigues18@gmail.com>
2023-05-04 18:52:37 -0300

    .

--- resources/views/user/premium.blade.php
+++ resources/views/user/premium.blade.php
@@ -17,10 +17,11 @@
         <div class="d-flex flex-row mt-1 flex-wrap justify-content-around">
             {{-- card-market --}}
             <div class="col-auto card-market flex-column text-center m-5 ">
-                <a class="btn" href="/" >
+                <a class="btn" href="/">
                     <div class="rounded-5 icon-link icon-link-hover" style="width: 20rem; height:max-content; background-color: #DBF0E1;">
-                        <i class=" fa-solid fa-shop" style="font-size: 10rem; margin-top:3rem;"></i>
+                        <img class="img-fluid" src="/img/gratuito.png" alt="" id="nav-free">
                         <h2 class="fs-3 fw-bold flex-column pb-3" style="margin-top:2rem">Gratuito</h2>
+                        <h3 class="fs-3 fw-bold  flex-column pb-3">Benefícios:</h3>
                     </div>
                 </a>
             </div>
@@ -29,8 +30,9 @@
             <div class=" col-auto card-user flex-column  text-center m-5">
                 <a class="btn" href="/premium">
                     <div class="rounded-5" style="width: 20rem; height:max-content; background-color: #DBF0E1">
-                        <i class="fa-solid fa-user" style="font-size: 10rem; margin-top:3rem;"></i>
-                        <h2 class="fs-3 fw-bold  flex-column pb-3" style="margin-top:2rem">Pacote Premium</h2>
+                        <img class="img-fluid" src="/img/premium.png" alt="" id="nav-premium">
+                        <h2 class="fs-3 fw-bold  flex-column pb-3" style="margin-top:2rem">Premium</h2>
+                        <h3 class="fs-3 fw-bold  flex-column pb-3">Benefícios:</h3>
                     </div>
                 </a>
             </div>
