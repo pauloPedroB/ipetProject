3480085e88cf445f552e5cf6dece11b9bac9a69f
paulopedrob <siqueirapedropaulo93@gmail.com>
2023-05-04 19:37:53 -0300

    .

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -111,11 +111,11 @@
                 <img src="/img/pet-cools/3906953.jpg" class="img-fluid rounded-4 w-100" alt="...">
             </div>
         </div>
-        <button class="carousel-control-prev" type="button" data-bs-target="#carouselIpet" data-bs-slide="prev">
+        <button class="carousel-control-prev m-5" type="button" data-bs-target="#carouselIpet" data-bs-slide="prev">
             <span class="carousel-control-prev-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Previous</span>
         </button>
-        <button class="carousel-control-next" type="button" data-bs-target="#carouselIpet" data-bs-slide="next">
+        <button class="carousel-control-next m-5" type="button" data-bs-target="#carouselIpet" data-bs-slide="next">
             <span class="carousel-control-next-icon" aria-hidden="true"></span>
             <span class="visually-hidden">Next</span>
         </button>
