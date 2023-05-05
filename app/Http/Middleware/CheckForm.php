c9c7a95c5f725cfe392fc21275535e05b446b233
pauloPedroB <117599481+pauloPedroB@users.noreply.github.com>
2023-05-05 07:54:39 -0300

    Update CheckForm.php

--- app/Http/Middleware/CheckForm.php
+++ app/Http/Middleware/CheckForm.php
@@ -1,1 +1,26 @@
-123
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Http\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class CheckForm
+{
+    /**
+     * Handle an incoming request.
+     *
+     * @param  \Closure(\Illuminate\Http\Request): (\Symfony\Component\HttpFoundation\Response)  $next
+     */
+    public function handle(Request $request, Closure $next): Response
+    {
+        if ($request->is('post') && 
+            ($request->is('multipart/form-data') || 
+             $request->is('application/x-www-form-urlencoded'))) {
+            return $next($request);
+        } else {
+            abort(403,'Não Autorizado');
+        }
+    }
+}
