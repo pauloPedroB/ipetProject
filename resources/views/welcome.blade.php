ccf5d0dec24560a2f3eb3a0d660b3516e06f201c
odair <odair.sp@hotmail.com>
2023-05-05 20:58:22 -0300

    atualiza header, offcanvas

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -18,68 +18,83 @@
 
 <body>
     <header>
-        <nav class="navbar navbar-expand-lg ">
-            <div>
-                <a href="" class="navbar-brand">
-                    <img class="img-fluid" src="/img/LogoIpet.png" alt="" id="nav-logo">
-                </a>
-            </div>
-            <div id="input-container">
-                <form action="/" method="GET">
-                    <input type="text" id="search" name="search" class="form-control"
-                        placeholder="Buscar Item ou Loja...">
-                    <div class="carousel-caption1">
-                        <select name="Category" id="Category">
-                            <option value="all">Todos</option>
-                            @foreach($categories as $category)
-                            <option value="{{$category->name}}">{{$category->name}}</option>
-                            @endforeach
 
-                        </select>
+        {{-- inicio --}}
+        <nav class="navbar navbar-expand-md">
+            <div class="container-fluid">
+
+                <div>
+                    <a href="" class="navbar-brand">
+                        <img class="img-fluid" src="/img/LogoIpet.png" alt="" id="nav-logo">
+                    </a>
+                </div>
+                <div id="input-container">
+                    <form action="/" method="GET">
+                        <input type="text" id="search" name="search" class="form-control"
+                            placeholder="Buscar Item ou Loja...">
+                        <div class="carousel-caption1">
+                            <select name="Category" id="Category">
+                                <option value="all">Todos</option>
+                                @foreach($categories as $category)
+                                <option value="{{$category->name}}">{{$category->name}}</option>
+                                @endforeach
+
+                            </select>
+                        </div>
+                    </form>
+                </div>
+                <div>
+                    <button id="btnToogle" class="navbar-toggler" type="button" data-bs-toggle="offcanvas"
+                        data-bs-target="#offcanvasNavbar" aria-controls="offcanvasNavbar"
+                        aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-start w-50" tabindex="-1" id="offcanvasNavbar"
+                        aria-labelledby="offcanvasNavbarLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="navbarNavLabel">iPet</h5>
+                            <button type="button" class="btn-close" data-bs-dismiss="offcanvas"
+                                aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <ul class="navbar-nav justify-content-end flex-grow-1 pe-3">
+                                @auth
+                                <li class="nav-item">
+                                    <a class="nav-link">Bem
+                                        Vindo,
+                                        {{stristr(Auth::user()->email,"@", true)}}.</a>
+                                </li>
+                                @endauth
+                                <li>
+                                    <a href="/" class="nav-link">Produtos</a>
+                                </li>
+                                @auth
+                                <li class="nav-item">
+                                    <a href="/dashboard" class="nav-link">Meus Dados</a>
+                                </li>
+                                <li class="nav-item">
+                                    <form action="/logout" method="POST">
+                                        @csrf
+                                        <input id="btnClose" type="submit"
+                                            onclick="product.preventDefault(); this.closest('form').submit();"
+                                            class="btnClose" value="Sair">
+                                    </form>
+                                </li>
+                                @endauth
+                                @guest
+                                <li class="nav-item">
+                                    <a href="/login" class="nav-link">Entrar</a>
+                                </li>
+
+                                @endguest
+                            </ul>
+
+                        </div>
                     </div>
-                </form>
-            </div>
-            <div>
-                <button id="btnToogle" class="navbar-toggler" type="button" data-bs-toggle="collapse"
-                    data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false"
-                    aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-            </div>
-            <div class="collapse navbar-collapse" id="navbarNav">
-                <ul class="navbar-nav">
-                    @auth
-                    <li class="nav-item">
-                        <a class="nav-link">Bem
-                            Vindo,
-                            {{stristr(Auth::user()->email,"@", true)}}.</a>
-                    </li>
-                    @endauth
-                    <li>
-                        <a href="/" class="nav-link">Produtos</a>
-                    </li>
-                    @auth
-                    <li class="nav-item">
-                        <a href="/dashboard" class="nav-link">Meus Dados</a>
-                    </li>
-                    <li class="nav-item">
-                        <form action="/logout" method="POST">
-                            @csrf
-                            <input id="btnClose" type="submit"
-                                onclick="product.preventDefault(); this.closest('form').submit();" class="btnClose"
-                                value="Sair">
-                        </form>
-                    </li>
-                    @endauth
-                    @guest
-                    <li class="nav-item">
-                        <a href="/login" class="nav-link">Entrar</a>
-                    </li>
-
-                    @endguest
-                </ul>
-            </div>
+                </div>
         </nav>
+        {{-- fim --}}
+
     </header>
 
     <div id="carouselIpet" class="carousel slide " data-bs-ride="carousel">
