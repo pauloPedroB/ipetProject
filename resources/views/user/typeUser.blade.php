198a1c133f97aa740afcf6b7fd14ef8e4eb90cb1
vinicius <vine.goncalo123@gmail.com>
2023-05-06 00:17:28 -0300

    apliquei animação nos botões de escolha e responsividade

--- resources/views/user/typeUser.blade.php
+++ resources/views/user/typeUser.blade.php
@@ -4,22 +4,23 @@
 
 
 {{-- container --}}
-<div class=" container-fluid" style="background-color: #F2F2F2">
+<div id="container_change" class=" container-fluid">
 
-    <div class="row " style="margin: 0">
+    <div id="row_change" class="row ">
         
         {{-- tittle --}}
-        <div class="p-5" style="color:#F29200; width: ">
-            <h1 class="">Você deseja se cadastrar como: </h1>
+        <div id="title_change" class="p-5">
+            <h1>Cadastre-se como:</h1>
         </div>
+
 
         <div class="d-flex flex-row mt-1 flex-wrap justify-content-around">
             {{-- card-market --}}
             <div class="col-auto card-market flex-column text-center m-5 ">
                 <a class="btn" href="/Registrar/Loja" >
-                    <div class="rounded-5 icon-link icon-link-hover" style="width: 20rem; height:max-content; background-color: #DBF0E1;">
-                        <i class=" fa-solid fa-shop" style="font-size: 10rem; margin-top:3rem;"></i>
-                        <h2 class="fs-3 fw-bold flex-column pb-3" style="margin-top:2rem">Loja</h2>
+                    <div id="card_store" class="icon-link">
+                        <span id="icon_store" class="material-symbols-outlined">store</span>
+                        <h2 id="title_store" class="fs-2 fw-bold flex-column pb-4">Loja</h2>
                     </div>
                 </a>
             </div>
@@ -27,9 +28,9 @@
             {{-- card-user --}}
             <div class=" col-auto card-user flex-column  text-center m-5">
                 <a class="btn" href="/Registrar/Usuario">
-                    <div class="rounded-5" style="width: 20rem; height:max-content; background-color: #DBF0E1">
-                        <i class="fa-solid fa-user" style="font-size: 10rem; margin-top:3rem;"></i>
-                        <h2 class="fs-3 fw-bold  flex-column pb-3" style="margin-top:2rem">Cliente</h2>
+                    <div id="card_user">
+                        <span id="icon_user" class="material-symbols-outlined">person</span>
+                        <h2 id="title_user" class="fs-2 fw-bold  flex-column pb-4" style="">Cliente</h2>
                     </div>
                 </a>
             </div>
