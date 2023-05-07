6729041c38abada0b843a4b8119b90b7c68bc26c
PEDRO BRAMBILA <pedro.brambila@etec.sp.gov.br>
2023-05-07 19:03:32 -0300

    a

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -23,12 +23,50 @@
                     autofocus autocomplete="username" />
             </div>
 
-            <div class="mt-4">
+            <div class="mt-4" id="password-input">
                 <x-label for="password" value="{{ __('Senha') }}" />
                 <x-input id="password" class="block mt-1 w-full" type="password" name="password" required
                     autocomplete="current-password" />
+                <button type="button" onclick="togglePasswordVisibility()" class="toggle-password"></button>
             </div>
+            <style>
+                #password-input {
+                    position: relative;
+                }
 
+                #password-input .toggle-password {
+                    position: absolute;
+                    top: 50%;
+                    right: 10px;
+                    transform: translateY(-50%);
+                    background-color: transparent;
+                    border: none;
+                    outline: none;
+                    cursor: pointer;
+                    width: 25px;
+                    height: 25px;
+                    background-image: url('https://cdn-icons-png.flaticon.com/512/69/69425.png');
+                    background-repeat: no-repeat;
+                    background-size: contain;
+                }
+
+                #password-input .toggle-password.hide {
+                    background-image: url('https://cdn-icons-png.flaticon.com/512/67/67553.png');
+                }
+            </style>
+            <script>
+                function togglePasswordVisibility() {
+                  var senhaInput = document.getElementById("password");
+                  var toggleBtn = document.querySelector(".toggle-password");
+                  if (senhaInput.type === "password") {
+                    senhaInput.type = "text";
+                    toggleBtn.classList.add("hide");
+                  } else {
+                    senhaInput.type = "password";
+                    toggleBtn.classList.remove("hide");
+                  }
+                }
+            </script>
             <div class="block mt-4">
                 <label for="remember_me" class="flex items-center">
                     <x-checkbox id="remember_me" name="remember" />
