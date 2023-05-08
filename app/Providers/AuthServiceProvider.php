d33a8e21d160d5060024faaa4f6cbd4917cf2056
paulopedrob <siqueirapedropaulo93@gmail.com>
2023-05-08 19:42:27 -0300

    att

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -25,21 +25,21 @@ class AuthServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        VerifyEmail::toMailUsing(function($notifiable,$url){
-            return (new MailMessage)
-                    ->subject('Verique seu E-mail')
-                    ->line('Por favor clique no link abaixo para verificar seu E-mail')
-                    ->action('Verifique seu E-mail',$url)
-                    ->line('Se você não criou uma conta, nenhuma ação é requerida!');
-        });
-        ResetPassword::toMailUsing(function($notifiable,$url){
-            $expires = config('auth.passwords.'.config('auth.defaults.passwords').'.expire');
-            return (new MailMessage)
-                    ->subject('Notificação de Reset de senha')
-                    ->line('Se você está recebendo este e-mail, é porque recebemos um pedido de reset de senha para sua conta')
-                    ->action('Resetar senha',$url)
-                    ->line('Este link de reset de senha espirará em: '.$expires.' minutos.')
-                    ->line('Se você não requisitou o reset, ignore está mensagem');
-        });
+       // VerifyEmail::toMailUsing(function($notifiable,$url){
+         //   return (new MailMessage)
+           //         ->subject('Verique seu E-mail')
+             //       ->line('Por favor clique no link abaixo para verificar seu E-mail')
+               //     ->action('Verifique seu E-mail',$url)
+                 //   ->line('Se você não criou uma conta, nenhuma ação é requerida!');
+      //  });
+        //ResetPassword::toMailUsing(function($notifiable,$url){
+          //  $expires = config('auth.passwords.'.config('auth.defaults.passwords').'.expire');
+            //return (new MailMessage)
+              //      ->subject('Notificação de Reset de senha')
+                //    ->line('Se você está recebendo este e-mail, é porque recebemos um pedido de reset de senha para sua conta')
+                  //  ->action('Resetar senha',$url)
+                    //->line('Este link de reset de senha espirará em: '.$expires.' minutos.')
+                    //->line('Se você não requisitou o reset, ignore está mensagem');
+        //});
     }
 }
