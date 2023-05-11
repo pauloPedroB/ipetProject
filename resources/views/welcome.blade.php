ff112effa9d7be5435bb27021b7796c66999273a
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-05-11 19:22:34 -0300

    att

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -221,8 +221,8 @@
     <script>
       var link = document.getElementById("nav-link");
       var linkText = link.innerText;
-      if (linkText.length > 12) {
-        link.innerText = linkText.substring(0, 12)+"...";
+      if (linkText.length > 5) {
+        link.innerText = linkText.substring(0, 5)+"...";
       }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
