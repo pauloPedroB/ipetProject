1a2db73e3322442d440f0e73433aacfcab0b6977
pauloPedroB <117599481+pauloPedroB@users.noreply.github.com>
2023-05-15 06:53:25 -0300

    Update copyProduct.blade.php

--- resources/views/products/copyProduct.blade.php
+++ resources/views/products/copyProduct.blade.php
@@ -186,7 +186,12 @@
     </footer>
 
     <script src="/js/app.js"></script>
-
+     <script>
+      const link = document.getElementById("nav-link");
+      if (link.innerText.length > 20) {
+        link.innerText = link.innerText.substring(0, 20)+"...";
+      }
+    </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
     </script>
