7e66014f1ffd8e7fae50b0f3d87cb74b554d6aaa
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-05-15 19:28:04 -0300

    att

--- app/Providers/AuthServiceProvider.php
+++ app/Providers/AuthServiceProvider.php
@@ -3,7 +3,7 @@
 namespace App\Providers;
 
 // use Illuminate\Support\Facades\Gate;
-
+use Jenssegers\Agent\Facades\Agent;
 use Illuminate\Auth\Notifications\ResetPassword;
 use Illuminate\Auth\Notifications\VerifyEmail;
 use Illuminate\Foundation\Support\Providers\AuthServiceProvider as ServiceProvider;
