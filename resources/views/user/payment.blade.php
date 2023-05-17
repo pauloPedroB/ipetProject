3b966390819ecdf77af2d0cdf7b995c862d30515
Maria Clara <mariaclarasilva9539@gmail.com>
2023-05-17 16:50:42 -0300

    Tela de pagamento em andamento

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -4,42 +4,53 @@
 
     {{-- container --}}
     <div class=" container-fluid" style="background-color: #ffff">
-
-        <h1>Escolha a forma de pagamento:</h1>
-        <div class="container-option">
-            <a href="/payment"><i class="fa-regular fa-credit-card"></i></a>
-            
-            <a href="/pix"><i class="fa-brands fa-pix"></i></a>
-            
-            <p>Débito/Crédito</p>
-        </div>
-        <div>
-            <form action="">
-                <label for="number">Nº do Cartão</label>
-                <input type="number" name="number" placeholder="XXXX XXXX XXXX XXXX">
-                <label for="name">Nome do Titular</label>
-                <input type="text" name="name">
-                <div>
-                    <label for="validity">Validade</label>
-                    <input type="month" placeholder="00/00">
-                    <label for="cvv"></label>
-                    <input type="number" name="cvv" min="3" max="3" placeholder="XXX">
-                </div>
-                <div>
-                    <input type="checkbox" checked>
-                    <p>Desejo deixar os dados do meu cartão salvo</p>
-                </div>
-                <div>
-                    <img class="img-fluid" src="/img/premium.png" alt="" >
-                    <h2>Premium</h2>
-                    <p>Descrição do Produto</p>
-                    <p>Total:R$ 00,00</p>
-                </div>
-                <button>Confirmar Pagamento</button>
-                <p>*Aprovação imediata*</p>
-            </form>
+        
+        <section id="content-payment">
+            <div class="payment-dados">
+                <form action="">
+                    <div class="card-dados" >
+                        <h1 class="title-payment">Escolha a forma de pagamento:</h1>
+                        <div class="container-option">
+                            <a href="/payment"><i class="fa-regular fa-credit-card" style="color: #ee5253;"></i></a>
+                            <p>Débito/Crédito</p>
+                            <a href="/pix"><i class="fa-brands fa-pix"></i></a>
+                            <p>PIX</p>
+                        </div>
+                        <label for="number">Nº do Cartão</label>
+                        <input type="number" name="number" placeholder="XXXX XXXX XXXX XXXX">
+                        <label for="name">Nome do Titular</label>
+                        <input type="text" name="name" placeholder="Maria Clara Santos">
+                    </div>
+                    <div class="form-card" >
+                        <div class="card-dados-validity">
+                            <label for="validity">Validade</label>
+                            <input type="month" placeholder="00/00">
+                        </div>
+                        <div class="card-dados-cvv">
+                            <label for="cvv">CVV</label>
+                            <input type="number" name="cvv" min="3" max="3" placeholder="XXX">
+                        </div>
+                    </div>
+                </form>
+                    <div class="card-checkbox">
+                        <input type="checkbox" checked>
+                        <p>Desejo deixar os dados do meu cartão salvo</p>
+                    </div>
+            </div>
+                    <div class="card-premium">
+                        <img class="img-premium" src="/img/premium.png" alt="" >
+                        <h2>Premium</h2>
+                        <p>Descrição do Produto</p>
+                        <p>Total:R$ 00,00</p>
+                    </div>
+        </section>
+        <div class="confirmation">
+            <button>Confirmar Pagamento</button>
+            <p>*Aprovação imediata*</p>
         </div>
     </div>
+               
+
     <script src="https://kit.fontawesome.com/02020a9349.js" crossorigin="anonymous"></script>
 
 @endsection
--- resources/views/user/pix.blade.php
+++ resources/views/user/pix.blade.php
@@ -2,6 +2,46 @@
 @section('title', 'Tipo de Usuário')
 @section('content')
 
-    <h1>Maria insuportável</h1>
+{{-- container --}}
+    <div class=" container-fluid" style="background-color: #ffff">
+        
+        <section id="content-payment">
+            <div class="payment-dados">
+                    <div class="pix-dados" >
+                        <h1 class="title-payment">Escolha a forma de pagamento:</h1>
+                        <div class="container-option">
+                            <a href="/payment"><i class="fa-regular fa-credit-card"></i></a>
+                            <p>Débito/Crédito</p>
+                            <a href="/pix"><i class="fa-brands fa-pix" style="color: #ee5253;"></i></a>
+                            <p>PIX</p>
+                        </div>
+                        <div class="pix-recebedor" >
+                            <h2>Recebedor</h2>
+                            <p>IPET</p>
+                            <h2>Código pix valido até: </h2>
+                            <p>23:59 horas</p>
+                        </div>
+                    </div>
+                    <div class="pix-codigo" >
+                        <span id="qrcode" class="material-symbols-outlined">qr_code_2</span>
+                        <p>000232056988044775006</p>
+                    </div>
+            </div>
+                    <div class="card-premium">
+                        <img class="img-premium" src="/img/premium.png" alt="" >
+                        <h2>Premium</h2>
+                        <p>Descrição do Produto</p>
+                        <p>Total:R$ 00,00</p>
+                    </div>
+        </section>
+        <div class="confirmation">
+            <button>Confirmar Pagamento</button>
+            <p>*Aprovação imediata*</p>
+        </div>
+    </div>
+               
+
+    <script src="https://kit.fontawesome.com/02020a9349.js" crossorigin="anonymous"></script>
+    <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
 
 @endsection
