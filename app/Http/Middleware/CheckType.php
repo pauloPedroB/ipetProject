e7b4b0de16223596649c75072b0c3a0eb4abd6f6
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-05-17 21:09:10 -0300

    att

--- app/Http/Middleware/CheckType.php
+++ app/Http/Middleware/CheckType.php
@@ -18,26 +18,37 @@ class CheckType
     public function handle(Request $request, Closure $next): Response
     {
         $User = auth()->user();
-        if($User)
-        {
-            $registro = Loja::where([
+        if($User){
+            $loja = Loja::where([
                 [
                     'user_id','=',$User->id
                 ]
-            ])->first();
-            if($registro == null){
-                $registro = Usuario::where([
+            ])->get();
+
+            if(count($loja)==0){
+                $loja = Usuario::where([
                     [
                         'user_id','=',$User->id
                     ]
-                ])->first();
+                ])->get();
+                if(count($loja)==0){
+                    $loja = null;
+                }
+               
             }
             
-            if($registro == null){
-                return redirect('/Tipo/Usuario');
-            }   
         }
-       
+        else{
+            $loja='teste';
+        }
+        
+        $end = false;
+        $id_end = 0;
+        if($loja==null && $User->AL_id !=3){
+            return redirect('/Tipo/Usuario');
+        }
+
+        
         return $next($request);
     }
 }
