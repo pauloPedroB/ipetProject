8f3f4a185083f78a1607d6e612c7269aa841bd2a
pauloPedroB <117599481+pauloPedroB@users.noreply.github.com>
2023-05-19 09:01:30 -0300

    Update LojaController.php

--- app/Http/Controllers/LojaController.php
+++ app/Http/Controllers/LojaController.php
@@ -92,14 +92,13 @@ class LojaController extends Controller
             [
                 'id','=',$id
             ]
-        ])->where('user_id','=',$user->id)
-             first();
+        ])->where('user_id','=',$user->id)->first();
         if($registro == null){
             $registro = Usuario::where([
                 [
                     'id','=',$id
                 ]
-            ])->first();
+            ])->where('user_id','=',$user->id)->first();
         }
         $endereco = Endereco::where([['id','=',$registro->Endereco_id]])->first();
         return view('user.lojaEdit',['registro'=>$registro,'endereco'=>$endereco,'user'=>$user]);
