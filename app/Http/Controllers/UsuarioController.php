1197851682a4b2ac6fefdc59ebd85bdd0e4e8547
pauloPedroB <117599481+pauloPedroB@users.noreply.github.com>
2023-05-19 08:23:55 -0300

    Update UsuarioController.php

--- app/Http/Controllers/UsuarioController.php
+++ app/Http/Controllers/UsuarioController.php
@@ -25,32 +25,7 @@ class UsuarioController extends Controller
      */
     public function create(Request $request)
     {
-        $cpf = preg_replace('/[^0-9]/', '', $request->CPF);
-    
-        if (strlen($cpf) !== 11) {
-            return redirect('/Registrar/Usuario')->with('error', 'O CPF Inválido');
-        }
-        if (preg_match('/(\d)\1{10}/', $cpf)) {
-            return redirect('/Registrar/Usuario')->with('error', 'O CPF Inválido');
-        }
-        $soma = 0;
-        for ($i = 0; $i < 9; $i++) {
-            $soma += intval($cpf[$i]) * (10 - $i);
-        }
-        $resto = $soma % 11;
-        $digito1 = ($resto < 2) ? 0 : (11 - $resto);
-        if (intval($cpf[9]) !== $digito1) {
-            return redirect('/Registrar/Usuario')->with('error', 'O CPF Inválido');
-        }
-        $soma = 0;
-        for ($i = 0; $i < 10; $i++) {
-            $soma += intval($cpf[$i]) * (11 - $i);
-        }
-        $resto = $soma % 11;
-        $digito2 = ($resto < 2) ? 0 : (11 - $resto);
-        if (intval($cpf[10]) !== $digito2) {
-            return redirect('/Registrar/Usuario')->with('error', 'O CPF Inválido');
-        }
+       
 
         $registro = Usuario::where([
             [
