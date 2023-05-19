f294bea2e82d499e98413096bc73df174d5a2bc9
pauloPedroB <117599481+pauloPedroB@users.noreply.github.com>
2023-05-19 08:16:49 -0300

    Update validations.php

--- app/Utils/validations.php
+++ app/Utils/validations.php
@@ -1,1 +1,21 @@
+<?php
 
+namespace App\Utils;
+
+class validations
+{
+    public static function validarNome($nome)
+    {
+        $nome = trim($nome);
+
+        if (strlen($nome) < 2 || strlen($nome) > 50) {
+            return 'O nome deve ter entre 2 e 50 caracteres.';
+        }
+
+        if (!preg_match('/^[a-zA-ZÀ-ú\s\'\-]+$/', $nome)) {
+            return 'O nome contém caracteres inválidos.';
+        }
+
+        return null; // Retorna nulo se o nome for válido
+    }
+}
