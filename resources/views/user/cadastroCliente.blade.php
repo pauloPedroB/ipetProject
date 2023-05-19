7fdbc3b39918130af1284fa557f5c199a05f458e
Ronald <ronalddantasrodrigues18@gmail.com>
2023-05-18 21:15:24 -0300

    Estilização

--- resources/views/user/cadastroCliente.blade.php
+++ resources/views/user/cadastroCliente.blade.php
@@ -5,111 +5,105 @@
     <div id="event-create-container" class="col-md-6 offset-md-3">
         
         <div id="header_form" class="d-flex flex-column flex-md-row align-items-center">
-            <i class=" col fa-solid fa-city  d-flex justify-content-center align-items-center m-" id="icon_register"></i>
+            <i class=" col fa-solid fa-user  d-flex justify-content-center align-items-center m-" id="icon_register"></i>
             <h1 class="fs-2 m-4">Cadastro - Pessoa Física</h1>
         </div>
-        <form class="d-flex flex-column flex-md-row align-items-center" action="/Cadastrar/Usuario" method="POST" enctype="multipart/form-data" id="addres">
+        <form class="d-flex flex-column flex-md-row align-items-baseline w-100 p-2 bs-light-rgb" action="/Cadastrar/Usuario" method="POST" enctype="multipart/form-data" id="addres">
             @csrf
             <div class="container-fluid " id="container-register">
                 
                 {{-- formulario  --}}
                         
-                    <div class="form-group">
-                        <label class="form-label"  for="cnpj">CPF:</label>
-                        @if (session('error'))
-                            <div class="mb-4 font-medium text-sm text-green-600">
-                                <label for="" id="error-message">{{ session('error') }}</label>
-                            </div>
-                        @endif
-                        <label for="" id="error-message"></label>
-                        <input class="form-control" type="text" name="CPF" id="CPF" class="" placeholder="__.___.___/____-__"required >
-                        
-                        <label class="form-label" for="nomeFantasia">Nome</label>
-                        <input class="form-control" type="text" id="Name" name="Name" required>
-                        
-                        <label class="form-label"  for="telefone">Telefone:</label>
-                        <input class="form-control" type="tel" id="Telefone" name="Telefone"required>
-                        
-                        <label class="form-label" for="celular">Celular:</label>
-                        <input class="form-control" type="tel" id="Celular" name="Celular"required>
-
-                        <label for="">Data de Nascimento:</label>
-                        <input type="date" name="DT" id="DT" required>
-                    </div>
+                <div class="form-group">
+                    <label class="form-label"  for="cnpj">CPF:</label>
+                    @if (session('error'))
+                        <div class="mb-4 font-medium text-sm text-green-600">
+                            <label for="" id="error-message">{{ session('error') }}</label>
+                        </div>
+                    @endif
+                    <label for="" id="error-message"></label>
+                    <input class="form-control" type="text" name="CPF" id="CPF" class="" placeholder="__.___.___/____-__"required >
                     
-            
+                    <label class="form-label" for="nomeFantasia">Nome</label>
+                    <input class="form-control" type="text" id="Name" name="Name" required>
+                    
+                    <label class="form-label"  for="telefone">Telefone:</label>
+                    <input class="form-control" type="tel" id="Telefone" name="Telefone"required>
+                    
+                    <label class="form-label" for="celular">Celular:</label>
+                    <input class="form-control" type="tel" id="Celular" name="Celular"required>
+
+                    <label for="">Data de Nascimento:</label>
+                    <input type="date" name="DT" id="DT" required>
+
+                    <br>
+
+                    <label class="form-label" for="title">CEP:</label>
+                    <input type="text" class="form-control" id="cep" name="cep" placeholder="Seu CEP..."  maxlength="8" required>
+                </div>
             </div>
 
-
             <div class="container-fluid " id="container-register">
-                <div id="message">
-                    <p></p>
-                </div>
-                    <div class="form-group">
-                        <label class="form-label" for="title">CEP:</label>
-                        <input type="text" class="form-control" id="cep" name="cep" placeholder="Seu CEP..."  maxlength="8" required>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label" for="title">Rua:</label>
-                        <input type="text" class="form-control" id="street" name="street" placeholder="Sua Rua..."  required>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label" for="title">Número</label>
-                        <input type="number" class="form-control" id="Number" name="Number"  required>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label" for="title">Bairro:</label>
-                        <input type="text" class="form-control" id="neighborhood" name="neighborhood" placeholder="Seu bairro..."  required>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label" for="title">Cidade</label>
-                        <textarea type="text" class="form-control" id="city" name="city" placeholder="Sua Cidade..." required>
-                           
-                        </textarea>
-                    </div>
-                    <div class="form-group">
-                        <label class="form-label" for="title">Estado</label>
-                        <select name="uf" id="uf"class="form-control" type="select" required data-input>
-                            <option selected>Estado</option>
-                            <option value="AL">AC</option>
-                            <option value="AL">AL</option>
-                            <option value="AP">AP</option>
-                            <option value="AM">AM</option>
-                            <option value="BA">BA</option>
-                            <option value="CE">CE</option>
-                            <option value="DF">DF</option>
-                            <option value="ES">ES</option>
-                            <option value="ES">ES</option>
-                            <option value="GO">GO</option>
-                            <option value="MA">MA</option>
-                            <option value="MT">MT</option>
-                            <option value="MS">MS</option>
-                            <option value="MG">MG</option>
-                            <option value="PA">PA</option>
-                            <option value="PB">PB</option>
-                            <option value="PR">PR</option>
-                            <option value="PE">PE</option>
-                            <option value="PI">PI</option>
-                            <option value="RJ">RJ</option>
-                            <option value="RN">RN</option>
-                            <option value="RS">RS</option>
-                            <option value="RO">RO</option>
-                            <option value="RR">RR</option>
-                            <option value="SC">SC</option>
-                            <option value="SP">SP</option>
-                            <option value="SE">SE</option>
-                            <option value="TO">TO</option>
-                        </select>
-                    </div>
-                    <div class="form-group">
+                <div class="form-group">
+                
+                    <label class="form-label" for="title">Rua:</label>
+                    <input type="text" class="form-control" id="street" name="street" placeholder="Sua Rua..."  required>
+                
+                    <label class="form-label" for="title">Número</label>
+                    <input type="number" class="form-control" id="Number" name="Number"  required>
+                
+                    <label class="form-label" for="title">Bairro:</label>
+                    <input type="text" class="form-control" id="neighborhood" name="neighborhood" placeholder="Seu bairro..."  required>
+                
+                    <label class="form-label" for="title">Cidade</label>
+                    <textarea type="text" class="form-control" id="city" name="city" placeholder="Sua Cidade..." required>
+                        
+                    </textarea>
+                
+                    <label class="form-label" for="title">Estado</label>
+                    <select name="uf" id="uf"class="form-control" type="select" required data-input>
+                        <option selected>Estado</option>
+                        <option value="AL">AC</option>
+                        <option value="AL">AL</option>
+                        <option value="AP">AP</option>
+                        <option value="AM">AM</option>
+                        <option value="BA">BA</option>
+                        <option value="CE">CE</option>
+                        <option value="DF">DF</option>
+                        <option value="ES">ES</option>
+                        <option value="ES">ES</option>
+                        <option value="GO">GO</option>
+                        <option value="MA">MA</option>
+                        <option value="MT">MT</option>
+                        <option value="MS">MS</option>
+                        <option value="MG">MG</option>
+                        <option value="PA">PA</option>
+                        <option value="PB">PB</option>
+                        <option value="PR">PR</option>
+                        <option value="PE">PE</option>
+                        <option value="PI">PI</option>
+                        <option value="RJ">RJ</option>
+                        <option value="RN">RN</option>
+                        <option value="RS">RS</option>
+                        <option value="RO">RO</option>
+                        <option value="RR">RR</option>
+                        <option value="SC">SC</option>
+                        <option value="SP">SP</option>
+                        <option value="SE">SE</option>
+                        <option value="TO">TO</option>
+                    </select>
+                    <div class="d-none">
                         <label class="form-label" for="title">Latitude</label>
                         <input type="text" class="form-control" id="lat" name="lat" placeholder="Sua Latitude..." disabled>
-                    </div>
-                    <div class="form-group">
+                    
                         <label class="form-label" for="title">Latitude</label>
                         <input type="text" class="form-control" id="long" name="long" placeholder="Sua Longitude..." disabled>
                     </div>
-                    <input type="submit" class="btn btn-primary" value="Registrar" id="mndEndereco">
+                </div>
+                <div class="d-flex flex-row justify-content-center">
+                    <button type="submit" class="btn btn-primary m-2" id="btnCadastro">Registrar</button>
+                    <a href="/Tipo/Usuario" class="btn btn-danger m-2" id="btnCadastro">Cancelar</a>
+                </div>
             </div>
         </form>
     </div>
