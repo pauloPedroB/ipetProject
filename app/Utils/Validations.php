9a340c493cc87853ee142a5c5462cb4190d3e5db
pauloPedroB <117599481+pauloPedroB@users.noreply.github.com>
2023-05-19 08:26:16 -0300

    Update Validations.php

--- app/Utils/Validations.php
+++ app/Utils/Validations.php
@@ -18,4 +18,34 @@ class Validations
 
         return null; // Retorna nulo se o nome for válido
     }
+    public static function validarCPF($cpf)
+    {
+            $cpf = preg_replace('/[^0-9]/', '', $request->CPF);
+
+            if (strlen($cpf) !== 11) {
+                return 'Erro';
+            }
+            if (preg_match('/(\d)\1{10}/', $cpf)) {
+                return 'Erro';
+            }
+            $soma = 0;
+            for ($i = 0; $i < 9; $i++) {
+                $soma += intval($cpf[$i]) * (10 - $i);
+            }
+            $resto = $soma % 11;
+            $digito1 = ($resto < 2) ? 0 : (11 - $resto);
+            if (intval($cpf[9]) !== $digito1) {
+                return 'Erro';
+            }
+            $soma = 0;
+            for ($i = 0; $i < 10; $i++) {
+                $soma += intval($cpf[$i]) * (11 - $i);
+            }
+            $resto = $soma % 11;
+            $digito2 = ($resto < 2) ? 0 : (11 - $resto);
+            if (intval($cpf[10]) !== $digito2) {
+                return 'Erro';
+            }
+            return null;
+    }
 }
