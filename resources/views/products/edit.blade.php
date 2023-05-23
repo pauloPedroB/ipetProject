4f2861f1e7f14661ac754893c600367f949183ca
PEDRO BRAMBILA <pedro.brambila@etec.sp.gov.br>
2023-05-23 00:24:10 -0300

    att

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -36,6 +36,7 @@
                 
 
                 <div id="dynamic-inputs">
+                    aaaaaaaaaaaaaaaaaaaaaaaaa
                     <!-- Aqui serão adicionados os novos inputs e labels -->
                     @foreach($description as $des)
                         @if (strpos($des, 'Idade: ') !== false)
