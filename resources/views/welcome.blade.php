89e7bbb585a3ebba09533abffec872b508b0f4a9
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-05-23 20:20:17 -0300

    att

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -245,7 +245,7 @@
       
       const titles = document.getElementById("card-title");
       if (titles.innerText.length > 29) {
-        titles.innerText = link.innerText.substring(0, 29)+"...";
+        titles.innerText = titles.innerText.substring(0, 29)+"...";
       }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
