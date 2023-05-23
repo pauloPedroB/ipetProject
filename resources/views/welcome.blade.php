e89ab6a06e7372bce9e6d950d79e01a39891d483
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-05-23 20:16:54 -0300

    att

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -242,6 +242,11 @@
       if (link.innerText.length > 20) {
         link.innerText = link.innerText.substring(0, 20)+"...";
       }
+      
+      const titles = document.getElementById("card-title");
+      if (link.innerText.length > 29) {
+        link.innerText = link.innerText.substring(0, 29)+"...";
+      }
     </script>
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0-alpha3/dist/js/bootstrap.bundle.min.js"
         integrity="sha384-ENjdO4Dr2bkBIFxQpeoTz1HIcje39Wm4jDKdf19U8gI4ddQ3GYNS7NTKfAdVQSZe" crossorigin="anonymous">
