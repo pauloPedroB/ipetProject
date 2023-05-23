bfc368bd8ad321ae4103d59a9d71bbf74b3edab0
pauloPedroB <117599481+pauloPedroB@users.noreply.github.com>
2023-05-23 09:04:35 -0300

    Update edit.blade.php

--- resources/views/products/edit.blade.php
+++ resources/views/products/edit.blade.php
@@ -19,7 +19,7 @@
                     <label for="title">Imagem do Produto: </label>
                     <input onchange="mostrarImagem()" value="/img/products/{{$product->Image}}" type="file" id="image" name="image" class="from-control-file" accept="image/png, image/jpeg" required>
                      <div id="imageforProduct">
-                        <img id="imagem-preview" src="#" alt="Imagem selecionada" style="width: 100px;">
+                        <img id="imagem-preview" src="/img/products/{{$product->Image}}" alt="Imagem selecionada" style="width: 100px;">
                     </div>
                 </div>
                 <div class="form-group">
