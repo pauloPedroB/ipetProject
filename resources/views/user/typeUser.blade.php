782c38aafb66501245ef887db743c14482340c82
Vinicius <vine.goncalo123@gmail.com>
2023-05-29 21:28:36 -0300

    att

--- resources/views/user/typeUser.blade.php
+++ resources/views/user/typeUser.blade.php
@@ -5,22 +5,21 @@
 
 
 {{-- container --}}
-<div class=" container-fluid">
+<div id="container-change" class=" container-fluid">
 
     <div id="row_change" class="row">
         
         {{-- tittle --}}
         <div id="title_change">
-            <h1>Cadastre-se como:</h1>
-            
+            <h1>Cadastre-se como:</h1>     
         </div>
 
 
-        <div class="d-flex flex-row mt-1 flex-wrap justify-content-around">
+        <div id="container-cards-change">
             {{-- card-market --}}
-            <div  class="col-auto flex-column text-center ">
+            <div id="card_store" class="col-auto flex-column text-center ">
                 <a class="btn" href="/Registrar/Loja" >
-                    <div class="card">
+                    <div class="cards">
                         <span id="icon_store" class="material-symbols-outlined">store</span>
                         <h2 id="title_store" class="fs-3 fw-bold flex-column pb-4">Loja</h2>
                     </div>
@@ -30,7 +29,7 @@
             {{-- card-user --}}
             <div class=" col-auto flex-column  text-center">
                 <a class="btn" href="/Registrar/Usuario">
-                    <div class="card">
+                    <div class="cards">
                         <span id="icon_user" class="material-symbols-outlined">person</span>
                         <h2 id="title_user" class="fs-3 fw-bold  flex-column pb-4" style="">Cliente</h2>
                     </div>
