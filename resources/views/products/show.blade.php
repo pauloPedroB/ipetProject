b8f886b254d1b62f30fd33c177f31993f395d0e8
pauloPedroB <siqueirapedropaulo93@gmail.com>
2023-06-01 21:13:02 -0300

    att

--- resources/views/products/show.blade.php
+++ resources/views/products/show.blade.php
@@ -1,5 +1,5 @@
 @extends('layouts.main')
-@section('title',{{$product->Name}})
+@section('title','.{{$product->Name}}.')
 @section('content')
 <div class="" id='show-main'>
     <div class="row" id="products-information">
