4614b5b7bb624c0f84e2b2068bc3948e108673d3
odairsp <odair.sp@hotmail.com>
2023-06-20 12:03:28 -0300

    att

--- resources/views/user/payment.blade.php
+++ resources/views/user/payment.blade.php
@@ -2,54 +2,61 @@
 @section('title', 'Tipo de Usuário')
 @section('content')
 
-    {{-- container --}}
-    <div class=" container" style="background-color: #ffff">
-        <div id="payment">
-                <section class="content-payment">
-                        <div class="card-premium">
-                            <img class="img-premium" src="/img/premium.png" alt="" >
-                            <h2>Premium</h2>
-                            <p>Seus produtos terão prioridade na divulgação!</p>
-                            <p style="font-size: 24px; color: #1db844">Assinatura Mensal: R$ 49,99</p>
-                            <p>Fidelidade de 1 ano.</p>
-                            <p>Total = R$ 499,00 com 16,5% de desconto</p>
+{{-- container --}}
+<div class=" container" style="background-color: #ffff">
+    <div id="payment">
+        <section class="content-payment">
+            <div class="card-premium">
+                <img class="img-premium" src="/img/premium.png" alt="">
+                <h2>Premium</h2>
+                <p>Seus produtos terão prioridade na divulgação!</p>
+                <p style="font-size: 24px; color: #1db844; font-weight: bold">Assinatura Mensal: R$ 49,99</p>
+                <p>Fidelidade de 1 ano.</p>
+                <p>Total = R$ 499,00 com 16,5% de desconto</p>
+            </div>
+            <div class="payment-dados">
+                <form action="/premium">
+                    <div class="card-dados">
+                        <h1 class="title-payment">Escolha a forma de pagamento:</h1>
+                        <div class="container-option">
+                            <a href="/payment"><i class="fa-regular fa-credit-card" style="color: #1db844;"></i></a>
+                            <p>Débito/Crédito</p>
+                            <a href="/pix"><i class="fa-brands fa-pix"></i></a>
+                            <p>PIX</p>
                         </div>
-                    <div class="payment-dados">
-                        <form action="/premium">
-                            <div class="card-dados" >
-                                <h1 class="title-payment">Escolha a forma de pagamento:</h1>
-                                <div class="container-option">
-                                    <a href="/payment"><i class="fa-regular fa-credit-card" style="color: #1db844;"></i></a>
-                                    <p>Débito/Crédito</p>
-                                    <a href="/pix"><i class="fa-brands fa-pix"></i></a>
-                                    <p>PIX</p>
-                                </div>
-                                <label for="number">Nº do Cartão</label>
-                                <input type="text"name="card_number" pattern="[0-9]{13,16}" placeholder="XXXX XXXX XXXX XXXX"required>
-                                <label for="name">Nome do Titular</label>
-                                <input type="text" name="name" placeholder="Maria Clara Santos" required>
-                            </div>
-                            <div class="form-card" >
-                                <div class="card-dados-validity">
-                                    <label for="validity">Validade</label>
-                                    <input type="month" placeholder="00/00" required>
-                                </div>
-                                <div class="card-dados-cvv">
-                                    <label for="cvv">CVV</label>
-                                    <input type="number" name="cvv"  maxlength="3" placeholder="XXX" required>
-                                </div>
-                            </div>
-                            <div class="confirmation">
-                                <button type="submit" name="confirmation">Confirmar Pagamento</button>
-                            </div>
-                        </form>
-                    </div>
-                  
-                </section>
-        </div>
-    </div>
-             
+                        <label for="number">Valor</label>
+                        <select name="premiun-value">
+                            <option value="Mensal = R$ 49,99"></option>
+                            <option value="Anual(-16,5%) = R$ 499,00"></option>
+                        </select>
 
-    <script src="https://kit.fontawesome.com/02020a9349.js" crossorigin="anonymous"></script>
+                        <label for="number">Nº do Cartão</label>
+                        <input type="text" name="card_number" pattern="[0-9]{13,16}" placeholder="XXXX XXXX XXXX XXXX"
+                            required>
+                        <label for="name">Nome do Titular</label>
+                        <input type="text" name="name" placeholder="Maria Clara Santos" required>
+                    </div>
+                    <div class="form-card">
+                        <div class="card-dados-validity">
+                            <label for="validity">Validade</label>
+                            <input type="month" placeholder="00/00" required>
+                        </div>
+                        <div class="card-dados-cvv">
+                            <label for="cvv">CVV</label>
+                            <input type="number" name="cvv" maxlength="3" placeholder="XXX" required>
+                        </div>
+                    </div>
+                    <div class="confirmation">
+                        <button type="submit" name="confirmation">Confirmar Pagamento</button>
+                    </div>
+                </form>
+            </div>
+
+        </section>
+    </div>
+</div>
+
+
+<script src="https://kit.fontawesome.com/02020a9349.js" crossorigin="anonymous"></script>
 
 @endsection
